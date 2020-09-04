0a2209aa5693393ca46176057a97a9ce6f5e2673
JikanBae <jikanbae@mgsoft.org>
2020-09-04 14:36:48 +0900

    Started the repository

---
+++ shbang.jl
@@ -0,0 +1,8 @@
+#!/Applications/Julia-1.5.app/Contents/Resources/julia/bin/julia
+
+# useage:
+# $ shbang.jl Foo Bar
+
+print("Hi, $(ARGS[1]). ")
+println("My name is $(ARGS[2]).")
+println("お元気ですか。")
